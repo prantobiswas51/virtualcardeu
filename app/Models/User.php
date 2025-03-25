b07b341657c4347cc771632686675b415954e3d2
Pranto Biswas <prantobd320@gmail.com>
2025-03-25 13:59:45 +0600

    error - filament

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,19 +3,33 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Panel;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->role === 'Admin';
+    }
 
     protected $fillable = [
         'name',
         'email',
         'password',
+        'paypal_email',
+        'paypal_id',
+        'profile_photo',
+        'pin',
+        'number',
+        'balance',
+        'role'
     ];
 
     protected $hidden = [
